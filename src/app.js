966d03a7ea9b5bb4da2fcfd8638d5b3abf636325
Kevin Wei <chenghaowei@gmail.com>
2019-09-26 21:58:22 -0400

    separate middleware and organize app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
+/*******************************************************************
+  IMPORTS
+*******************************************************************/
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validateBearerToken');
+
+/*******************************************************************
+  INIT
+*******************************************************************/
 const app = express();
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
-
-app.use(morgan(morganOption));
+/*******************************************************************
+  MIDDLEWARE
+*******************************************************************/
+app.use(morgan(NODE_ENV === 'production' ? 'tiny' : 'common'));
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
 
+/*******************************************************************
+  ROUTES
+*******************************************************************/
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  return res.send('Hello, world!');
 });
 
+/*******************************************************************
+  ERROR HANDLING
+*******************************************************************/
 app.use(function errorHandler(err, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
@@ -24,7 +40,10 @@ app.use(function errorHandler(err, req, res, next) {
     console.error(err);
     response = { message: err.message, error: err };
   }
-  res.status(500).json(response);
+  return res.status(500).json(response);
 });
 
+/*******************************************************************
+  EXPORTS
+*******************************************************************/
 module.exports = app;
