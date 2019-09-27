e6ea86872dccd25621a562e90ecf59187c991f65
Kevin Wei <chenghaowei@gmail.com>
2019-09-26 22:06:57 -0400

    separate errorHandler

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const validateBearerToken = require('./validateBearerToken');
+const errorHandler = require('./errorHandler');
 
 /*******************************************************************
   INIT
@@ -32,16 +33,7 @@ app.get('/', (req, res) => {
 /*******************************************************************
   ERROR HANDLING
 *******************************************************************/
-app.use(function errorHandler(err, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(err);
-    response = { message: err.message, error: err };
-  }
-  return res.status(500).json(response);
-});
+app.use(errorHandler);
 
 /*******************************************************************
   EXPORTS
