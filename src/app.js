215f49c982d903f604510b120064b14a25e07b14
Kevin Wei <chenghaowei@gmail.com>
2019-10-04 22:56:09 -0400

    move files to bin, rename expect to chai.expect

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const validateBearerToken = require('./validateBearerToken');
-const errorHandler = require('./errorHandler');
+const validateBearerToken = require('./bin/validateBearerToken');
+const errorHandler = require('./bin/errorHandler');
 
 /*******************************************************************
   INIT
